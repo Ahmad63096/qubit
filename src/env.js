992e0282849efe96e927e1aeb8efa10526b48082
ahmad hassan <ah9007935@gmail.com>
2024-12-13 19:30:43 +0500

    update API

--- src/env.js
+++ src/env.js
@@ -1,7 +1,6 @@
 // const url = 'https://devbot.devspandas.com/devbot/chat';
-const url = 'http://16.170.135.224:5000/v1/devbot/chat';
-const greatingMessageurl = 'https://dashboard.devspandas.com/api/config/get_greetings_message';
-//const url = 'http://localhost:8000/chat';
-// const url = 'http://20.20.20.19:8000/chat';
-// const url = 'http://20.20.20.59:8000/chat';
+// const url = 'http://16.170.135.224:5000/v1/devbot/chat';
+const url = 'https://bot.devspandas.com/v1/devbot/chat';
+// const greatingMessageurl = 'https://dashboard.devspandas.com/api/config/get_greetings_message';
+const greatingMessageurl = 'https://bot.devspandas.com/api/config/get_greetings_message';
 export { url, greatingMessageurl };
